20362332add1825a89f4b993bd7a42a77227740e
zanaptak <53196138+zanaptak@users.noreply.github.com>
2019-09-27 22:03:21 -0500

    Add NextBytes overload and NextBoolean, use ctor overload for buffer size, use 53 bits for NextDouble

--- src/BufferedCryptoRandom.fs
+++ src/BufferedCryptoRandom.fs
@@ -3,14 +3,15 @@ namespace Zanaptak.BufferedCryptoRandom
 open System
 open System.Security.Cryptography
 
-type BufferedCryptoRandom private ( bufferByteCount : uint16 ) =
+/// Creates an instance with the specified buffer size in bytes (min 16, max 65536).
+type BufferedCryptoRandom ( bufferByteCount : int ) =
   inherit Random ()
 
   let cryptoRng = RandomNumberGenerator.Create()
 
-  static let [< Literal >] defaultBufferLength = 512us
+  static let [< Literal >] defaultBufferLength = 256
 
-  let bufferLength = max bufferByteCount 32us |> int
+  let bufferLength = max bufferByteCount 16 |> min 65536
   let byteBuffer : byte array = Array.zeroCreate bufferLength
   let mutable bufferPos = bufferLength
 
@@ -21,16 +22,21 @@ type BufferedCryptoRandom private ( bufferByteCount : uint16 ) =
   let ensureBuffer numBytes =
     if bufferLength - bufferPos < numBytes then refillBuffer()
 
-  let nextBytes ( bytes : byte array ) =
-    if bytes = null then raise ( ArgumentException( "byte array cannot be null" ) )
-    if bytes.Length = 0 then ()
-    elif bytes.Length >= bufferLength then cryptoRng.GetBytes( bytes )
-    else
+  let nextBytes ( bytes : byte array ) startIndex length =
+    if length < bufferLength then
       lock byteBuffer.SyncRoot ( fun () ->
-        ensureBuffer bytes.Length
-        Buffer.BlockCopy ( byteBuffer , bufferPos , bytes , 0 , bytes.Length )
-        bufferPos <- bufferPos + bytes.Length
+        ensureBuffer length
+        Buffer.BlockCopy ( byteBuffer , bufferPos , bytes , startIndex , length )
+        bufferPos <- bufferPos + length
       )
+    else
+      // Requesting more bytes than in buffer; fill from cryptoRng.
+      if length = bytes.Length then
+        cryptoRng.GetBytes( bytes )
+      else
+        let tempBytes = Array.create length 0uy
+        cryptoRng.GetBytes( tempBytes )
+        Buffer.BlockCopy ( tempBytes , 0 , bytes , startIndex , length )
 
   let nextUInt8 () : uint8 =
     lock byteBuffer.SyncRoot ( fun () ->
@@ -92,167 +98,220 @@ type BufferedCryptoRandom private ( bufferByteCount : uint16 ) =
     let r = nextUInt64 ()
     if r >= Int64Threshold then r % Int64Bound |> int64 else nextInt64 ()
 
-  let nextBoundedUInt8 ( bound : uint8 ) : uint8 =
-    if bound = 0uy then 0uy
-    else
-      let threshold = ( uint8 -( int8 bound ) ) % bound
+  let nextBoundedUInt8 ( maxExclusive : uint8 ) : uint8 =
+    if maxExclusive > 0uy then
+      let threshold = ( uint8 -( int8 maxExclusive ) ) % maxExclusive
       let rec loop () =
         let r = nextUInt8 ()
-        if r >= threshold then r % bound else loop ()
+        if r >= threshold then r % maxExclusive else loop ()
       loop ()
+    else 0uy
 
-  let nextBoundedInt8 ( maxExc : int8 ) =
-    if maxExc < 0y then raise ( ArgumentException( "maxExclusive cannot be less than 0" ) )
-    else nextBoundedUInt8 ( uint8 maxExc ) |> int8
-
-  let nextRangeUInt8 ( minInc : uint8 ) ( maxExc : uint8 ) =
-    if minInc > maxExc then raise ( ArgumentException( "minInclusive cannot be greater than maxExclusive" ) )
-    elif minInc = maxExc then minInc
+  let nextBoundedInt8 ( maxExclusive : int8 ) =
+    if maxExclusive >= 0y then
+      nextBoundedUInt8 ( uint8 maxExclusive ) |> int8
     else
-      let bound = maxExc - minInc
-      minInc + nextBoundedUInt8 bound
+      raise ( ArgumentException( "maxExclusive cannot be less than 0" ) )
 
-  let nextRangeInt8 ( minInc : int8 ) ( maxExc : int8 ) =
-    if minInc > maxExc then raise ( ArgumentException( "minInclusive cannot be greater than maxExclusive" ) )
-    elif minInc = maxExc then minInc
-    else
-      let bound = uint8 ( maxExc - minInc ) // math works due to overflow
-      minInc + ( nextBoundedUInt8 bound |> int8 )
+  let nextRangeUInt8 ( minInclusive : uint8 ) ( maxExclusive : uint8 ) =
+    if maxExclusive > minInclusive then
+      let bound = maxExclusive - minInclusive
+      minInclusive + nextBoundedUInt8 bound
+    elif minInclusive = maxExclusive then minInclusive
+    else raise ( ArgumentException( "minInclusive cannot be greater than maxExclusive" ) )
 
-  let nextBoundedUInt16 ( bound : uint16 ) =
-    if bound = 0us then 0us
-    else
-      let threshold = ( uint16 -( int16 bound ) ) % bound
+  let nextRangeInt8 ( minInclusive : int8 ) ( maxExclusive : int8 ) =
+    if maxExclusive > minInclusive then
+      let bound = uint8 ( maxExclusive - minInclusive ) // math works due to overflow
+      minInclusive + ( nextBoundedUInt8 bound |> int8 )
+    elif minInclusive = maxExclusive then minInclusive
+    else raise ( ArgumentException( "minInclusive cannot be greater than maxExclusive" ) )
+
+  let nextBoundedUInt16 ( maxExclusive : uint16 ) =
+    if maxExclusive > 0us then
+      let threshold = ( uint16 -( int16 maxExclusive ) ) % maxExclusive
       let rec loop () =
         let r = nextUInt16 ()
-        if r >= threshold then r % bound else loop ()
+        if r >= threshold then r % maxExclusive else loop ()
       loop ()
+    else 0us
 
-  let nextBoundedInt16 ( maxExc : int16 ) =
-    if maxExc < 0s then raise ( ArgumentException( "maxExclusive cannot be less than 0" ) )
-    else nextBoundedUInt16 ( uint16 maxExc ) |> int16
-
-  let nextRangeUInt16 ( minInc : uint16 ) ( maxExc : uint16 ) =
-    if minInc > maxExc then raise ( ArgumentException( "minInclusive cannot be greater than maxExclusive" ) )
-    elif minInc = maxExc then minInc
+  let nextBoundedInt16 ( maxExclusive : int16 ) =
+    if maxExclusive >= 0s then
+      nextBoundedUInt16 ( uint16 maxExclusive ) |> int16
     else
-      let bound = maxExc - minInc
-      minInc + nextBoundedUInt16 bound
+      raise ( ArgumentException( "maxExclusive cannot be less than 0" ) )
 
-  let nextRangeInt16 ( minInc : int16 ) ( maxExc : int16 ) =
-    if minInc > maxExc then raise ( ArgumentException( "minInclusive cannot be greater than maxExclusive" ) )
-    elif minInc = maxExc then minInc
-    else
-      let bound = uint16 ( maxExc - minInc ) // math works due to overflow
-      minInc + ( nextBoundedUInt16 bound |> int16 )
+  let nextRangeUInt16 ( minInclusive : uint16 ) ( maxExclusive : uint16 ) =
+    if maxExclusive > minInclusive then
+      let bound = maxExclusive - minInclusive
+      minInclusive + nextBoundedUInt16 bound
+    elif minInclusive = maxExclusive then minInclusive
+    else raise ( ArgumentException( "minInclusive cannot be greater than maxExclusive" ) )
 
-  let nextBoundedUInt32 ( bound : uint32 ) =
-    if bound = 0u then 0u
-    else
-      let threshold = ( uint32 -( int32 bound ) ) % bound
+  let nextRangeInt16 ( minInclusive : int16 ) ( maxExclusive : int16 ) =
+    if maxExclusive > minInclusive then
+      let bound = uint16 ( maxExclusive - minInclusive ) // math works due to overflow
+      minInclusive + ( nextBoundedUInt16 bound |> int16 )
+    elif minInclusive = maxExclusive then minInclusive
+    else raise ( ArgumentException( "minInclusive cannot be greater than maxExclusive" ) )
+
+  let nextBoundedUInt32 ( maxExclusive : uint32 ) =
+    if maxExclusive > 0u then
+      let threshold = ( uint32 -( int32 maxExclusive ) ) % maxExclusive
       let rec loop () =
         let r = nextUInt32 ()
-        if r >= threshold then r % bound else loop ()
+        if r >= threshold then r % maxExclusive else loop ()
       loop ()
+    else 0u
 
-  let nextBoundedInt32 ( maxExc : int32 ) =
-    if maxExc < 0 then raise ( ArgumentException( "maxExclusive cannot be less than 0" ) )
-    else nextBoundedUInt32 ( uint32 maxExc ) |> int32
-
-  let nextRangeUInt32 ( minInc : uint32 ) ( maxExc : uint32 ) =
-    if minInc > maxExc then raise ( ArgumentException( "minInclusive cannot be greater than maxExclusive" ) )
-    elif minInc = maxExc then minInc
+  let nextBoundedInt32 ( maxExclusive : int32 ) =
+    if maxExclusive >= 0 then
+      nextBoundedUInt32 ( uint32 maxExclusive ) |> int32
     else
-      let bound = maxExc - minInc
-      minInc + nextBoundedUInt32 bound
+      raise ( ArgumentException( "maxExclusive cannot be less than 0" ) )
 
-  let nextRangeInt32 ( minInc : int32 ) ( maxExc : int32 ) =
-    if minInc > maxExc then raise ( ArgumentException( "minInclusive cannot be greater than maxExclusive" ) )
-    elif minInc = maxExc then minInc
-    else
-      let bound = uint32 ( maxExc - minInc ) // math works due to overflow
-      minInc + ( nextBoundedUInt32 bound |> int32 )
+  let nextRangeUInt32 ( minInclusive : uint32 ) ( maxExclusive : uint32 ) =
+    if maxExclusive > minInclusive then
+      let bound = maxExclusive - minInclusive
+      minInclusive + nextBoundedUInt32 bound
+    elif minInclusive = maxExclusive then minInclusive
+    else raise ( ArgumentException( "minInclusive cannot be greater than maxExclusive" ) )
 
-  let nextBoundedUInt64 ( bound : uint64 ) =
-    if bound = 0UL then 0UL
-    else
-      let threshold = ( uint64 -( int64 bound ) ) % bound
+  let nextRangeInt32 ( minInclusive : int32 ) ( maxExclusive : int32 ) =
+    if maxExclusive > minInclusive then
+      let bound = uint32 ( maxExclusive - minInclusive ) // math works due to overflow
+      minInclusive + ( nextBoundedUInt32 bound |> int32 )
+    elif minInclusive = maxExclusive then minInclusive
+    else raise ( ArgumentException( "minInclusive cannot be greater than maxExclusive" ) )
+
+  let nextBoundedUInt64 ( maxExclusive : uint64 ) =
+    if maxExclusive > 0UL then
+      let threshold = ( uint64 -( int64 maxExclusive ) ) % maxExclusive
       let rec loop () =
         let r = nextUInt64 ()
-        if r >= threshold then r % bound else loop ()
+        if r >= threshold then r % maxExclusive else loop ()
       loop ()
+    else 0UL
 
-  let nextBoundedInt64 ( maxExc : int64 ) =
-    if maxExc < 0L then raise ( ArgumentException( "maxExclusive cannot be less than 0" ) )
-    else nextBoundedUInt64 ( uint64 maxExc ) |> int64
-
-  let nextRangeUInt64 ( minInc : uint64 ) ( maxExc : uint64 ) =
-    if minInc > maxExc then raise ( ArgumentException( "minInclusive cannot be greater than maxExclusive" ) )
-    elif minInc = maxExc then minInc
+  let nextBoundedInt64 ( maxExclusive : int64 ) =
+    if maxExclusive >= 0L then
+      nextBoundedUInt64 ( uint64 maxExclusive ) |> int64
     else
-      let bound = maxExc - minInc
-      minInc + nextBoundedUInt64 bound
+      raise ( ArgumentException( "maxExclusive cannot be less than 0" ) )
 
-  let nextRangeInt64 ( minInc : int64 ) ( maxExc : int64 ) =
-    if minInc > maxExc then raise ( ArgumentException( "minInclusive cannot be greater than maxExclusive" ) )
-    elif minInc = maxExc then minInc
-    else
-      let bound = uint64 ( maxExc - minInc ) // math works due to overflow
-      minInc + ( nextBoundedUInt64 bound |> int64 )
+  let nextRangeUInt64 ( minInclusive : uint64 ) ( maxExclusive : uint64 ) =
+    if maxExclusive > minInclusive then
+      let bound = maxExclusive - minInclusive
+      minInclusive + nextBoundedUInt64 bound
+    elif minInclusive = maxExclusive then minInclusive
+    else raise ( ArgumentException( "minInclusive cannot be greater than maxExclusive" ) )
 
-  let nextDouble () =
-    let rec loop () =
-      let r = nextUInt32 ()
-      if r < UInt32.MaxValue then float r / float UInt32.MaxValue
-      else loop ()
-    loop ()
+  let nextRangeInt64 ( minInclusive : int64 ) ( maxExclusive : int64 ) =
+    if maxExclusive > minInclusive then
+      let bound = uint64 ( maxExclusive - minInclusive ) // math works due to overflow
+      minInclusive + ( nextBoundedUInt64 bound |> int64 )
+    elif minInclusive = maxExclusive then minInclusive
+    else raise ( ArgumentException( "minInclusive cannot be greater than maxExclusive" ) )
 
   static let globalInstance = BufferedCryptoRandom()
   static member Global = globalInstance
 
-  /// Creates an instance with a custom buffer size. Minimum value of 32 will be used if provided value is lower.
-  static member Create( bufferByteCount : uint16 ) = BufferedCryptoRandom( bufferByteCount )
-
-  /// Creates an instance with a 512-byte buffer. Seed is ingored.
-  new( seedIgnored : int ) = BufferedCryptoRandom()
-  /// Creates an instance with a 512-byte buffer.
+  /// Creates an instance with a 256-byte buffer.
   new() = BufferedCryptoRandom( defaultBufferLength )
 
+  /// Returns a random 32-bit signed integer greater than or equal to 0 and less than Int32.MaxValue.
   override this.Next() = nextInt32 ()
+  /// Returns a random 32-bit signed integer less than the specified maximum.
   override this.Next( maxExclusive : int ) = nextBoundedInt32 maxExclusive
+  /// Returns a random 32-bit signed integer greater than or equal to the specified minimum and less than the specified maximum.
   override this.Next( minInclusive : int , maxExclusive : int ) = nextRangeInt32 minInclusive maxExclusive
 
-  override this.NextBytes( bytes ) = nextBytes bytes
-  override this.NextDouble() = nextDouble ()
+  //  http://prng.di.unimi.it/
+  //  A standard double (64-bit) floating-point number in IEEE floating point format has 52 bits of significand, plus an implicit bit at the left of the significand. Thus, the representation can actually store numbers with 53 significant binary digits.
+  //  Because of this fact, in C99 a 64-bit unsigned integer x should be converted to a 64-bit double using the expression
+  //    #include <stdint.h>
+  //    (x >> 11) * (1. / (UINT64_C(1) << 53))
 
+  /// Returns a random double greater than or equal to 0.0 and less than 1.0.
+  override this.NextDouble() =
+    float ( nextUInt64 () >>> 11 ) * 0.00000000000000011102230246251565
+
+  /// Sets all bytes in the specified array to random bytes.
+  override this.NextBytes( bytes : byte array ) =
+    if ( not ( isNull bytes ) ) && bytes.Length > 0 then
+      nextBytes bytes 0 bytes.Length
+    elif isNull bytes then
+      raise ( ArgumentException( "byte array cannot be null" ) )
+    else ()
+
+  /// Sets the specified bytes in the specified array to random bytes.
+  member this.NextBytes( bytes : byte array , startIndex , length ) =
+    if ( not ( isNull bytes ) ) && startIndex >= 0 && length > 0 && ( startIndex + length ) <= bytes.Length then
+      nextBytes bytes startIndex length
+    elif isNull bytes then
+      raise ( ArgumentException( "byte array cannot be null" ) )
+    elif startIndex < 0 || startIndex >= bytes.Length then
+      raise ( ArgumentOutOfRangeException( "start index must be within array bounds" ) )
+    elif ( startIndex + length ) > bytes.Length then
+      raise ( ArgumentException( "length parameter must not exceed number of elements from start index to end of array" ) )
+    else ()
+
+  /// Returns a random boolean.
+  member this.NextBoolean() = nextUInt8 () >= 128uy
+
+  /// Returns a random 8-bit signed integer greater than or equal to 0 and less than SByte.MaxValue.
   member this.NextInt8() = nextInt8 ()
+  /// Returns a random 8-bit signed integer less than the specified maximum.
   member this.NextInt8( maxExclusive : int8 ) = nextBoundedInt8 maxExclusive
+  /// Returns a random 8-bit signed integer greater than or equal to the specified minimum and less than the specified maximum.
   member this.NextInt8( minInclusive : int8 , maxExclusive : int8 ) = nextRangeInt8 minInclusive maxExclusive
 
+  /// Returns a random 8-bit unsigned integer greater than or equal to 0 and less than or equal to Byte.MaxValue.
   member this.NextUInt8() = nextUInt8 ()
+  /// Returns a random 8-bit unsigned integer less than the specified maximum.
   member this.NextUInt8( maxExclusive : uint8 ) = nextBoundedUInt8 maxExclusive
+  /// Returns a random 8-bit unsigned integer greater than or equal to the specified minimum and less than the specified maximum.
   member this.NextUInt8( minInclusive : uint8 , maxExclusive : uint8 ) = nextRangeUInt8 minInclusive maxExclusive
 
+  /// Returns a random 16-bit signed integer greater than or equal to 0 and less than Int16.MaxValue.
   member this.NextInt16() = nextInt16 ()
+  /// Returns a random 16-bit signed integer less than the specified maximum.
   member this.NextInt16( maxExclusive : int16 ) = nextBoundedInt16 maxExclusive
+  /// Returns a random 16-bit signed integer greater than or equal to the specified minimum and less than the specified maximum.
   member this.NextInt16( minInclusive : int16 , maxExclusive : int16 ) = nextRangeInt16 minInclusive maxExclusive
 
+  /// Returns a random 16-bit unsigned integer greater than or equal to 0 and less than or equal to UInt16.MaxValue.
   member this.NextUInt16() = nextUInt16 ()
+  /// Returns a random 16-bit unsigned integer less than the specified maximum.
   member this.NextUInt16( maxExclusive : uint16 ) = nextBoundedUInt16 maxExclusive
+  /// Returns a random 16-bit unsigned integer greater than or equal to the specified minimum and less than the specified maximum.
   member this.NextUInt16( minInclusive : uint16 , maxExclusive : uint16 ) = nextRangeUInt16 minInclusive maxExclusive
 
+  /// Returns a random 32-bit signed integer greater than or equal to 0 and less than Int32.MaxValue.
   member this.NextInt32() = nextInt32 ()
+  /// Returns a random 32-bit signed integer less than the specified maximum.
   member this.NextInt32( maxExclusive : int32 ) = nextBoundedInt32 maxExclusive
+  /// Returns a random 32-bit signed integer greater than or equal to the specified minimum and less than the specified maximum.
   member this.NextInt32( minInclusive : int32 , maxExclusive : int32 ) = nextRangeInt32 minInclusive maxExclusive
 
+  /// Returns a random 32-bit unsigned integer greater than or equal to 0 and less than or equal to UInt32.MaxValue.
   member this.NextUInt32() = nextUInt32 ()
+  /// Returns a random 32-bit unsigned integer less than the specified maximum.
   member this.NextUInt32( maxExclusive : uint32 ) = nextBoundedUInt32 maxExclusive
+  /// Returns a random 32-bit unsigned integer greater than or equal to the specified minimum and less than the specified maximum.
   member this.NextUInt32( minInclusive : uint32 , maxExclusive : uint32 ) = nextRangeUInt32 minInclusive maxExclusive
 
+  /// Returns a random 64-bit signed integer greater than or equal to 0 and less than Int64.MaxValue.
   member this.NextInt64() = nextInt64 ()
+  /// Returns a random 64-bit signed integer less than the specified maximum.
   member this.NextInt64( maxExclusive : int64 ) = nextBoundedInt64 maxExclusive
+  /// Returns a random 64-bit signed integer greater than or equal to the specified minimum and less than the specified maximum.
   member this.NextInt64( minInclusive : int64 , maxExclusive : int64 ) = nextRangeInt64 minInclusive maxExclusive
 
+  /// Returns a random 64-bit unsigned integer greater than or equal to 0 and less than or equal to UInt64.MaxValue.
   member this.NextUInt64() = nextUInt64 ()
+  /// Returns a random 64-bit unsigned integer less than the specified maximum.
   member this.NextUInt64( maxExclusive : uint64 ) = nextBoundedUInt64 maxExclusive
+  /// Returns a random 64-bit unsigned integer greater than or equal to the specified minimum and less than the specified maximum.
   member this.NextUInt64( minInclusive : uint64 , maxExclusive : uint64 ) = nextRangeUInt64 minInclusive maxExclusive
